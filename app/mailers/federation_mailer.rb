45f04af85cbe5568cb8a04b41606cd5e4885a86a
NathanofZion <38050940+Nathanofzion@users.noreply.github.com>
2019-01-17 20:42:57 +0000

    Update federation_mailer.rb

--- app/mailers/federation_mailer.rb
+++ app/mailers/federation_mailer.rb
@@ -4,7 +4,7 @@
 #
 # MIT License
 #
-# Copyright (c) 2017-2018 Cryptomover
+# Copyright (c) 2018-2019 Zioncoin Foundation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
